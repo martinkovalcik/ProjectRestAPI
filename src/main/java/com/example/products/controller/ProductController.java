5c8c74cb27f44f1d84f6e2a65b4704ae63755fef
martinkovalcik <koviyolo@gmailcom>
2021-03-18 12:13:49 +0100

    Get done

--- src/main/java/com/example/products/controller/ProductController.java
+++ src/main/java/com/example/products/controller/ProductController.java
@@ -1,5 +1,68 @@
 package com.example.products.controller;
 
+
+import com.example.products.ProductsApplication;
+import com.example.products.model.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    @GetMapping("/list")
+    public List<Product> getAllMovies() {
+        Product product;
+        List<Product> list = new ArrayList<>();
+
+        try {
+            PreparedStatement statement = ProductsApplication.getConnection().prepareStatement(
+                    "select * from products");
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                product = new Product();
+                product.setId(rs.getInt("id"));
+                product.setName(rs.getString("name"));
+                product.setDescription(rs.getString("description"));
+                product.setPrice(rs.getDouble("price"));
+
+                list.add(product);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    @GetMapping(path = "/{id}")
+    public Product getProductById(@PathVariable int id) {
+        Product product = new Product();
+
+        try {
+            PreparedStatement statement = ProductsApplication.getConnection().prepareStatement(
+                    "select * from products where id = ?");
+            statement.setInt(1, id);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                product = new Product();
+                product.setId(id);
+                product.setName(rs.getString("name"));
+                product.setDescription(rs.getString("description"));
+                product.setPrice(rs.getDouble("price"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return product;
+    }
 
 }
---
+++ src/main/java/com/example/products/model/Product.java
@@ -0,0 +1,70 @@
+package com.example.products.model;
+
+import javax.persistence.*;
+
+@Entity(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "price")
+    private double price;
+
+    public Product() {
+    }
+
+    public Product(int id, String name, String description, double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
